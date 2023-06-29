2d1655405f80f93f245ea56ab833f09c45e8dbaa
tidzu <114362597+tidzu@users.noreply.github.com>
2023-06-29 14:41:04 +0200

    Done

---
+++ Others/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"KlientBanku"},{"p":"<Unnamed>","l":"KlientBankuBlik"},{"p":"<Unnamed>","l":"KlientBankuDowod"},{"p":"<Unnamed>","l":"KlientBankuKarta"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
